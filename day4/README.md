678c8d26656610d9cf52d8c908748914afad86a4
Neil Gall <neilgall@mac.com>
2019-12-04 07:14:57 +0000

    Day 4 part 1

---
+++ day4/README.md
@@ -0,0 +1,11 @@
+# Day 4
+
+Ah, I've been hoping for a constraint solving problem so I can try to remember Prolog.
+
+This runs in the GNU Prolog REPL with:
+
+```
+	?- [day4].
+	?- main(X).
+```
+
---
+++ day4/day4.pl
@@ -0,0 +1,45 @@
+
+digit(0).
+digit(1).
+digit(2).
+digit(3).
+digit(4).
+digit(5).
+digit(6).
+digit(7).
+digit(8).
+digit(9).
+
+in_range(A, B, C, D, E, F) :-
+	 X = (((((A * 10 + B) * 10 + C) * 10) + D) * 10 + E) * 10 + F
+   , 272091 =< X
+   , X =< 815432.
+
+has_adjacent_digits_the_same(A, B, C, D, E, F) :-
+	  A == B
+	; B == C
+	; C == D
+	; D == E
+	; E == F.
+
+has_no_decreasing_digits(A, B, C, D, E, F) :-
+	  A =< B
+	, B =< C
+	, C =< D
+	, D =< E
+	, E =< F.
+
+solve(A, B, C, D, E, F) :-
+	  digit(A)
+	, digit(B)
+	, digit(C)
+	, digit(D)
+	, digit(E)
+	, digit(F)
+	, has_adjacent_digits_the_same(A, B, C, D, E, F)
+	, has_no_decreasing_digits(A, B, C, D, E, F)
+	, in_range(A, B, C, D, E, F).
+
+main(Count) :-
+	  setof([A,B,C,D,E,F], solve(A, B, C, D, E, F), L)
+	, length(L, Count).
