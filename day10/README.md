f3c93d0b95507e02f555d2083fb5b28ac54eb49d
Neil Gall <neilgall@mac.com>
2019-12-10 23:44:05 +0000

    Day 10 part 2

--- day10/README.md
+++ day10/README.md
@@ -9,8 +9,13 @@ I scribbled on my [reMarkable](https://remarkable.com) tablet all the way to wor
 		for each remaining asteroid A
 			get the offset from C to A
 			if this is vertical, the step is 0,1 or 0,-1 depending on direction
-			else divide the offset by the GCD of offset-x and offset-y
+			else the step is the offset divided by the GCD of its x and y
 			trace away from A to the edge of space by the calculated step
 				eliminate any asteroids seen at the step locations
 
-Seems to work, and my Python solution runs in 1.3 seconds.
+Seems to work, I think it's O(N²), and my Python solution runs in 1.3 seconds.
+
+
+## Part 2
+
+Part 2 was completely different, and the above integer approach wouldn't work at all. I had to convert the offsets to each asteroid from the station to polar coordinates, group the asteroids with the same angle, then iterate over the groups removing the closest asteroid each time. Horrible, horrible problem!
--- day10/day10.py
+++ day10/day10.py
@@ -1,12 +1,16 @@
+from itertools import groupby
+from math import atan2, cos, pi, sin, sqrt
 
 def load(text):
   """
   Given a textual map, yields all the asteroid coordinates
   """
-  for y, row in enumerate(text.strip().split("\n")):
-    for x, cell in enumerate(row):
-      if cell == '#':
-        yield (x, y)
+  def generate():
+    for y, row in enumerate(text.strip().split("\n")):
+      for x, cell in enumerate(row):
+        if cell == '#':
+          yield (x, y)
+  return list(generate())
 
 
 def space_size(asteroids):
@@ -19,6 +23,28 @@ def space_size(asteroids):
   miny = min(y for x,y in asteroids)
   maxy = max(y for x,y in asteroids)
   return range(minx, maxx+1), range(miny, maxy+1)
+
+
+def space_perimeter(xrange, yrange, x):
+  """
+  Given the space size and an x coordinate, yield the list of
+  coordinates which form the space perimiter in clockwise order
+  starting directly above x
+  """
+  minx = min(xrange)
+  maxx = max(xrange)
+  miny = min(yrange)
+  maxy = max(yrange)
+  for x in range(x, maxx+1):
+    yield (x,miny)
+  for y in yrange[1:]:
+    yield (y,maxx)
+  for x in xrange[::-1]:
+    yield (x,maxy)
+  for y in yrange[::-1]:
+    yield (minx, y)
+  for x in range(minx, x):
+    yield (x,miny)
 
 
 def sq_distance_from(x1,y1):
@@ -39,12 +65,24 @@ def gcd(x,y):
   return y if x == 0 else gcd(y % x, x)
 
 
+def step_offset(sx,sy, ex,ey):
+  """
+  Calculate the step offset (x,y) to move from (sx,sy) to (ex,ey)
+  hitting all integer locations in between
+  """
+  if ey == sy:
+    return (1,0) if ex > sx else (-1,0)
+  else:
+    ox,oy = ex-sx, ey-sy
+    min_step = gcd(abs(ox),abs(oy))
+    return ox//min_step, oy//min_step
+
+
 def best_location(asteroids):
   """
   Given a list of asteroids, determine the asteroid which can "see"
   the most other asteroids, and the number it can see
   """
-  asteroids = list(asteroids)
   xrange, yrange = space_size(asteroids)
   best = None
   best_visible = 0
@@ -56,14 +94,7 @@ def best_location(asteroids):
     for ax,ay in sorted(asteroids, key=sq_distance_from(cx,cy)):
       if (ax,ay) not in visible:
         continue
-      if ay == cy:
-        ox,oy = (1,0) if ax > cx else (-1,0)
-      else:
-        ox,oy = ax-cx,ay-cy
-        min_step = gcd(abs(ox),abs(oy))
-        ox //= min_step
-        oy //= min_step
-      
+      ox, oy = step_offset(cx,cy,ax,ay)
       px,py = ax+ox,ay+oy
       while visible and px in xrange and py in yrange:
         if (px,py) in visible:
@@ -76,6 +107,48 @@ def best_location(asteroids):
       best = cx, cy
 
   return (best, best_visible)
+
+
+def vaporise_asteroids(asteroids, station):
+  """
+  Given the asteroid list and monitoring station location, yield the
+  vaporised asteroids in order of destruction
+  """
+  def to_polar(x, y):
+    """
+    Convert cartesian coordinates to polar, rotated so that theta=0
+    is directly upwards
+    """
+    theta = atan2(x, y)
+    rho = sqrt(x**2 + y**2)
+    return theta, rho
+
+
+  def to_cartesian(polar):
+    """
+    Convert polar to cartesian coordinates, assuming theta=0 is
+    directly upwards
+    """
+    theta, rho = polar
+    oy, ox = round(rho * cos(theta)), round(rho * sin(theta))
+    return sx + ox, sy + oy
+
+
+  asteroids = (a for a in asteroids if a != station)
+  sx, sy = station
+
+  asteroids_polar = sorted((to_polar(ax - sx, ay - sy) for ax, ay in asteroids), reverse=True)
+  asteroids_grouped = list((k,list(v)) for k,v in groupby(asteroids_polar, lambda a: a[0]))
+
+  while asteroids_grouped != []:
+    for theta, group in list(asteroids_grouped):
+      a = to_cartesian(group[-1])
+      yield a
+      if len(group) == 1:
+        asteroids_grouped.remove((theta,group))
+      else:
+        del group[0]
+
 
 tests = [
 ((3, 4), 8, """
@@ -145,12 +218,49 @@ tests = [
 """)
 ]
 
-if __name__ == "__main__":
+part2_test_map = """
+.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##
+"""
+
+def part1_tests():
   for pos, count, map in tests:
     p, c = best_location(load(map))
     assert (p,c) == (pos, count)
 
+def part2_tests():
+  v = vaporise_asteroids(load(part2_test_map), (8,3))
+  assert next(v) == (8,1)
+  assert next(v) == (9,0)
+  assert next(v) == (9,1)
+  assert next(v) == (10,0)
+  assert next(v) == (9,2)
+  assert next(v) == (11,1)
+  assert next(v) == (12,1)
+  assert next(v) == (11,2)
+  assert next(v) == (15,1)
+
+
+def part1(input):
+  pos, count = best_location(input)
+  return count
+
+
+def part2(input):
+  station, count = best_location(input)
+  zap = vaporise_asteroids(input, station)
+  x, y = list(zap)[199]
+  return x*100+y
+
+if __name__ == "__main__":
+  part1_tests()
+  part2_tests()
+
   with open("input.txt") as f:
  	  input = load(f.read())
 
-  print(f"Part 1... {best_location(input)}")
+  print(f"Part 1... {part1(input)}")
+  print(f"Part 1... {part2(input)}")
