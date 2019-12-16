536fc3717ad913cccc6eae67ada663afdf955604
Neil Gall <neilgall@mac.com>
2019-12-16 09:21:23 +0000

    Revert to Strings

--- day16/day16.hs
+++ day16/day16.hs
@@ -1,18 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import Data.Char (isSpace, ord)
 import Data.List (dropWhileEnd, intercalate, transpose)
 import Test.Hspec
-import qualified Data.Text as T
 
 basePattern = [0,1,0,-1]
 zeroChar = ord '0'
 
-parseInput :: T.Text -> [Int]
-parseInput = map (flip (-) zeroChar . ord) . T.unpack
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
 
-showOutput :: [Int] -> T.Text
-showOutput = T.intercalate "" . map (T.pack . show)
+parseInput :: String -> [Int]
+parseInput = map (flip (-) zeroChar . ord)
+
+showOutput :: [Int] -> String
+showOutput = intercalate "" . map show
 
 patternForIndex :: Int -> [Int]
 patternForIndex n = drop 1 $ concat $ repeat $ concat $ transpose $ replicate n basePattern
@@ -27,14 +27,14 @@ outputRow input n = (abs $ sum $ map pairProduct $ zip input (patternForIndex n)
 fftPhase :: [Int] -> Int -> [Int]
 fftPhase input _ = map (outputRow input) [1..length input]
 
-runPhases :: Int -> T.Text -> T.Text
+runPhases :: Int -> String -> String
 runPhases n input = showOutput $ foldl fftPhase (parseInput input) [1..n]
 
-extractMessage :: T.Text -> T.Text
-extractMessage input = T.take 8 $ T.drop offset $ fftOutput
+extractMessage :: String -> String
+extractMessage input = take 8 $ drop offset $ fftOutput
   where
-    offset = read (T.unpack $ T.take 7 input) :: Int
-    realSignal = T.replicate 10000 input
+    offset = read (take 7 input) :: Int
+    realSignal = concat $ replicate 10000 input
     fftOutput = runPhases 100 realSignal
 
 
@@ -43,21 +43,17 @@ fftPhaseTests = do
   map showOutput phases `shouldBe` ["12345678", "48226158", "34040438", "03415518", "01029498"]
 
 largerTests = do
-  T.take 8 (runPhases 100 "80871224585914546619083218645595") `shouldBe` "24176176"
-  T.take 8 (runPhases 100 "19617804207202209144916044189917") `shouldBe` "73745418"
-  T.take 8 (runPhases 100 "69317163492948606335995924319873") `shouldBe` "52432133"
-
-messageTests = do
-  extractMessage "03036732577212944063491565474664" `shouldBe` "84462026"
+  runPhases 100 "80871224585914546619083218645595" `shouldStartWith` "24176176"
+  runPhases 100 "19617804207202209144916044189917" `shouldStartWith` "73745418"
+  runPhases 100 "69317163492948606335995924319873" `shouldStartWith` "52432133"
 
 part1 input =
-  let result = T.take 8 $ runPhases 100 input in
-  putStrLn $ "Part 1 .. " ++ (show result)
+  let result = take 8 $ runPhases 100 input in
+  putStrLn $ "Part 1 .. " ++ result
 
 main = do
   fftPhaseTests
   largerTests
-  -- messageTests
 
-  input <- fmap (T.strip . T.pack) $ readFile "input.txt"
+  input <- fmap strip $ readFile "input.txt"
   part1 input
